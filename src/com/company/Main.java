adbbc8a7ac80ce528e17c4b00c76f1c541641d29
sparrow84 <18346183461q>
2018-08-15 21:41:32 +0700

    Start

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Test frame");
+    }
+}
