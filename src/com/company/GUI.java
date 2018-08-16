4090fc2dc8e84f7efa884e93b178395044f6defc
sparrow84 <18346183461q>
2018-08-17 00:38:08 +0700

    Start

---
+++ src/com/company/GUI.java
@@ -0,0 +1,7 @@
+package com.company;
+
+import javax.swing.*;
+
+public class GUI extends JFrame {
+    // public  BorderLayoutTest() {}
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,26 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Main {
+//                     createGUI
+    public static void createGUI() {
+        JFrame frame = new JFrame("Test frame 11111111");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JLabel label = new JLabel("Test label 22222222");
+        frame.getContentPane().add(label);
+        frame.setPreferredSize(new Dimension(200,100));
+        frame.pack();
+        frame.setVisible(true);
+    }
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Test frame");
+//        JFrame.setDefaultLookAndFeelDecorated(true);
+        javax.swing.SwingUtilities.invokeLater(
+            new Runnable() {
+                public void run() {
+                    createGUI();
+                }
+            }
+        );
     }
+
 }
